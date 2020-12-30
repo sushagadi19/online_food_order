026014e40618d1c318cbfedb0075b6b1eb809920
sushagadi19 <50733312+sushagadi19@users.noreply.github.com>
2020-12-30 18:37:01 +0530

    Add files via upload

---
+++ ecommerce/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+
+]
+urlpatterns = [
+ path('', TemplateView.as_view(template_name="social_app/index.html")),
+ path('admin/', admin.site.urls),
+ path('accounts/', include('allauth.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
